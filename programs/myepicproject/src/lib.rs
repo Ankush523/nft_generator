6d55a6b39e5289a9dd4ad13b97f510ad01aa9329
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-07-07 20:20:21 +0530

    complete

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("GEJcReVMo4jq8Z7uF5gQkZTEShmsddbs2ohVH9LjKc9c");
 
 #[program]
 pub mod myepicproject {
